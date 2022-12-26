556141dcc05e1459f7d84599ffd9b3ac2af89153
Jony Hayama <jonathan.hayama@gmail.com>
2022-12-26 17:02:25 -0300

    ✨ Add main game mechanism

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,35 @@
+<script setup>
+import { ref } from "vue";
+
+const MAX_BINGO_NUMBERS = 10;
+function randomIntFromInterval(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min)
+}
+
+const drawnNumbers = ref([]);
+
+const drawNumber = () => {
+  if (drawnNumbers.value.length >= MAX_BINGO_NUMBERS) {
+    alert('All numbers have been drawn, no bingo??');
+    return;
+  }
+
+  let number;
+  do {
+    number = randomIntFromInterval(1, MAX_BINGO_NUMBERS);
+  } while (drawnNumbers.value.includes(number))
+
+  drawnNumbers.value.push(number);
+}
+
+</script>
+
 <template>
-  <div>BINGO</div>
+  <div>
+    <button type="button" @click="drawNumber">Draw Number</button>
+
+    <ul v-if="drawnNumbers.length > 0">
+      <li v-for="number in drawnNumbers">{{ number }}</li>
+    </ul>
+  </div>
 </template>
