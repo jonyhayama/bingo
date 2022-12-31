1731bcca7eabd82d6c62c07f7c9e0f80a83677ec
Jony Hayama <jonathan.hayama@gmail.com>
2022-12-31 06:17:30 -0300

    🚸 Show all possible numbers and activate the drawn ones

--- src/components/GameResults.vue
+++ src/components/GameResults.vue
@@ -1,6 +1,6 @@
 <script setup>
 import { defineProps, computed, toRefs } from 'vue';
-import { formatNumber } from "@/util";
+import { MAX_BINGO_NUMBERS, formatNumber } from "@/util";
 
 const props = defineProps({
   game: Array
@@ -12,11 +12,13 @@ const orderedNumbers = computed(() => {
   const collator = new Intl.Collator(undefined, {numeric: true, sensitivity: 'base'});
   return [ ...game.value ].sort(collator.compare);
 })
+
+const allNumbers = [...Array(MAX_BINGO_NUMBERS).keys()].map(n => n + 1)
 </script>
 
 <template>
   <ul>
-    <li v-for="number in orderedNumbers">{{ formatNumber(number) }}</li>
+    <li v-for="number in allNumbers" :class="{ active: game.includes(number) }">{{ formatNumber(number) }}</li>
   </ul>
 </template>
 
@@ -30,17 +32,22 @@ ul {
   li {
     list-style: none;
     margin: 0.25em;
-    background: var(--color);
-    color: var(--background-color);
+    background: var(--form-element-disabled-background-color);
     width: 2em;
     height: 2em;
     border-radius: 50%;
     display: flex;
     align-items: center;
     justify-content: center;
+    transition: all .3s ease-in-out;
 
     &::marker {
       content: none;
+    }
+
+    &.active {
+      background: var(--color);
+      color: var(--background-color);
     }
   }
 }
